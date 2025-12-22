cdbdcb561949486ff02706df89274d9037760e47
antonia43 <199650822+antonia43@users.noreply.github.com>
2025-12-22 20:41:44 +0000

    Update student_career_quiz.py

--- cctesting-main/careercorner2/pages/student_career_quiz.py
+++ cctesting-main/careercorner2/pages/student_career_quiz.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from langfuse_helper import LangfuseGeminiWrapper, get_user_id, get_session_id
-from database import save_report
+from services.langfuse_helper import LangfuseGeminiWrapper, get_user_id, get_session_id
+from utils.database import save_report
 from datetime import datetime
 import os
 import re
